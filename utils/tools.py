176da692e8bd370d7e17379c22c3d1eecda5de15
Eloik <443345680@qq.com>
2023-06-30 21:07:54 +0800

    some tools

---
+++ utils/tools.py
@@ -0,0 +1,102 @@
+from typing import Optional
+import torch
+from tqdm import tqdm
+from torchvision.utils import make_grid
+from PIL import Image
+
+
+def train_one_epoch(trainer, loader, optimizer, device, epoch):
+    trainer.train()
+    total_loss, total_num = 0., 0
+
+    with tqdm(loader, dynamic_ncols=True, colour="#ff924a") as data:
+        for images, _ in data:
+            optimizer.zero_grad()
+
+            x_0 = images.to(device)
+            loss = trainer(x_0)
+
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+            total_num += x_0.shape[0]
+
+            data.set_description(f"Epoch: {epoch}")
+            data.set_postfix(ordered_dict={
+                "train_loss": total_loss / total_num,
+            })
+
+    return total_loss / total_num
+
+
+def save_image(images: torch.Tensor, nrow: int = 8, show: bool = True, path: Optional[str] = None,
+               format: Optional[str] = None, to_grayscale: bool = False, **kwargs):
+    """
+    concat all image into a picture.
+
+    Parameters:
+        images: a tensor with shape (batch_size, channels, height, width).
+        nrow: decide how many images per row. Default `8`.
+        show: whether to display the image after stitching. Default `True`.
+        path: the path to save the image. if None (default), will not save image.
+        format: image format. You can print the set of available formats by running `python3 -m PIL`.
+        to_grayscale: convert PIL image to grayscale version of image. Default `False`.
+        **kwargs: other arguments for `torchvision.utils.make_grid`.
+
+    Returns:
+        concat image, a tensor with shape (height, width, channels).
+    """
+    images = images * 0.5 + 0.5
+    grid = make_grid(images, nrow=nrow, **kwargs)  # (channels, height, width)
+    #  (height, width, channels)
+    grid = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+
+    im = Image.fromarray(grid)
+    if to_grayscale:
+        im = im.convert(mode="L")
+    if path is not None:
+        im.save(path, format=format)
+    if show:
+        im.show()
+    return grid
+
+
+def save_sample_image(images: torch.Tensor, show: bool = True, path: Optional[str] = None,
+                      format: Optional[str] = None, to_grayscale: bool = False, **kwargs):
+    """
+    concat all image including intermediate process into a picture.
+
+    Parameters:
+        images: images including intermediate process,
+            a tensor with shape (batch_size, sample, channels, height, width).
+        show: whether to display the image after stitching. Default `True`.
+        path: the path to save the image. if None (default), will not save image.
+        format: image format. You can print the set of available formats by running `python3 -m PIL`.
+        to_grayscale: convert PIL image to grayscale version of image. Default `False`.
+        **kwargs: other arguments for `torchvision.utils.make_grid`.
+
+    Returns:
+        concat image, a tensor with shape (height, width, channels).
+    """
+    images = images * 0.5 + 0.5
+
+    grid = []
+    for i in range(images.shape[0]):
+        # for each sample in batch, concat all intermediate process images in a row
+        t = make_grid(images[i], nrow=images.shape[1], **kwargs)  # (channels, height, width)
+        grid.append(t)
+    # stack all merged images to a tensor
+    grid = torch.stack(grid, dim=0)  # (batch_size, channels, height, width)
+    grid = make_grid(grid, nrow=1, **kwargs)  # concat all batch images in a different row, (channels, height, width)
+    #  (height, width, channels)
+    grid = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+
+    im = Image.fromarray(grid)
+    if to_grayscale:
+        im = im.convert(mode="L")
+    if path is not None:
+        im.save(path, format=format)
+    if show:
+        im.show()
+    return grid
