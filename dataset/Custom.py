c090a55e7c86c3f18c1ee8b82af011b6ab459d01
Eloik <443345680@qq.com>
2023-07-01 14:05:36 +0800

    add custom dataset

---
+++ dataset/Custom.py
@@ -0,0 +1,57 @@
+from torch.utils.data import DataLoader, Dataset
+from pathlib2 import Path
+from typing import Union, Iterable
+from PIL import Image
+from torchvision import transforms
+
+
+class ImageDataset(Dataset):
+    def __init__(self, path: Union[str, Path], prefix: Iterable[str] = ("png", "jpg"),
+                 mode: str = "RGB", transform=None):
+        super().__init__()
+
+        if isinstance(path, str):
+            path = Path(path)
+
+        self.images = []
+        for m in prefix:
+            self.images += list(path.glob(f"*.{m}"))
+
+        if mode not in ["RGB", "L", "CMYK"]:
+            raise ValueError("mode must be one of {'RGB', 'L', 'CMYK'}")
+        self.mode = mode
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, item):
+        image_path = str(self.images[item])
+        image = Image.open(image_path).convert(self.mode)
+
+        if self.transform is not None:
+            image = self.transform(image)
+        return image
+
+
+def create_custom_dataset(data_path, batch_size, **kwargs):
+    norm = (0.5,) if kwargs.get("mode", "RGB") == "L" else (0.5, 0.5, 0.5)
+    trans = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(norm, norm)
+    ])
+    dataset_param = dict(
+        prefix=kwargs.get("prefix", ("png", "jpg")),
+        mode=kwargs.get("mode", "RGB"),
+    )
+    dataset = ImageDataset(data_path, transform=trans, **dataset_param)
+
+    loader_params = dict(
+        shuffle=kwargs.get("shuffle", True),
+        drop_last=kwargs.get("drop_last", True),
+        pin_memory=kwargs.get("pin_memory", True),
+        num_workers=kwargs.get("num_workers", 4),
+    )
+    dataloader = DataLoader(dataset, batch_size=batch_size, **loader_params)
+    return dataloader
