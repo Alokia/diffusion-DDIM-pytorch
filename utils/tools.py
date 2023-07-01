201024e396036eea6b2d704d744b8e6389414e22
Eloik <443345680@qq.com>
2023-07-01 14:05:47 +0800

    add load yaml func

--- utils/tools.py
+++ utils/tools.py
@@ -1,8 +1,18 @@
-from typing import Optional
+from typing import Optional, Union
 import torch
 from tqdm import tqdm
 from torchvision.utils import make_grid
 from PIL import Image
+from pathlib2 import Path
+import yaml
+
+
+def load_yaml(yml_path: Union[Path, str], encoding="utf-8"):
+    if isinstance(yml_path, str):
+        yml_path = Path(yml_path)
+    with yml_path.open('r', encoding=encoding) as f:
+        cfg = yaml.load(f.read(), Loader=yaml.SafeLoader)
+        return cfg
 
 
 def train_one_epoch(trainer, loader, optimizer, device, epoch):
