7399aae5e7541f0643993a433b1041068a0930fe
Eloik <443345680@qq.com>
2023-07-01 14:05:25 +0800

    unified api

---
+++ dataset/__init__.py
@@ -0,0 +1,14 @@
+from dataset.MNIST import create_mnist_dataset
+from dataset.CIFAR import create_cifar100_dataset
+from dataset.Custom import create_custom_dataset
+
+
+def create_dataset(dataset: str, **kwargs):
+    if dataset == "mnist":
+        return create_mnist_dataset(**kwargs)
+    elif dataset == "cifar":
+        return create_cifar100_dataset(**kwargs)
+    elif dataset == "custom":
+        return create_custom_dataset(**kwargs)
+    else:
+        raise ValueError(f"dataset except one of {'mnist', 'cifar', 'custom'}, but got {dataset}")
