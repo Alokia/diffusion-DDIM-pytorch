8e5b3c8ea50202c7c9687566643b34e8d8cc0c39
Eloik <443345680@qq.com>
2023-07-01 15:13:17 +0800

    add consume

--- train.py
+++ train.py
@@ -3,20 +3,37 @@ from model.UNet import UNet
 from utils.engine import GaussianDiffusionTrainer
 from utils.tools import train_one_epoch, load_yaml
 import torch
+from utils.callbacks import ModelCheckpoint
 
 
 def train(config):
+    consume = config["consume"]
+    if consume:
+        cp = torch.load(config["consume_path"])
+        config = cp["config"]
+    print(config)
+
     device = torch.device(config["device"])
-
     loader = create_dataset(**config["Dataset"])
-    model = UNet(**config["Model"]).to(device)
+    start_epoch = 1
 
+    model = UNet(**config["Model"]).to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=config["lr"], weight_decay=1e-4)
     trainer = GaussianDiffusionTrainer(model, **config["Trainer"]).to(device)
 
-    for epoch in range(1, config["epochs"] + 1):
+    model_checkpoint = ModelCheckpoint(**config["Callback"])
+
+    if consume:
+        model.load_state_dict(cp["model"])
+        optimizer.load_state_dict(cp["optimizer"])
+        model_checkpoint.load_state_dict(cp["model_checkpoint"])
+        start_epoch = cp["start_epoch"] + 1
+
+    for epoch in range(start_epoch, config["epochs"] + 1):
         loss = train_one_epoch(trainer, loader, optimizer, device, epoch)
-        torch.save(model.state_dict(), config["save_path"])
+        model_checkpoint.step(loss, model=model.state_dict(), config=config,
+                              optimizer=optimizer.state_dict(), start_epoch=start_epoch,
+                              model_checkpoint=model_checkpoint.state_dict())
 
 
 if __name__ == "__main__":
