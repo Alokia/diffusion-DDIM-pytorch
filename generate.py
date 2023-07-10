b80e4b522c06ee554ab6603818afe122bc0187f6
Eloik <443345680@qq.com>
2023-07-10 17:37:09 +0800

    add DDIM sampler

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-from utils.engine import DDPMSampler
+from utils.engine import DDPMSampler, DDIMSampler
 from model.UNet import UNet
 import torch
 from utils.tools import save_sample_image, save_image
@@ -9,6 +9,7 @@ def parse_option():
     parser = ArgumentParser()
     parser.add_argument("-cp", "--checkpoint_path", type=str, required=True)
     parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--sampler", type=str, default="ddpm", choices=["ddpm", "ddim"])
 
     # generator param
     parser.add_argument("-bs", "--batch_size", type=int, default=16)
@@ -16,6 +17,11 @@ def parse_option():
     # sampler param
     parser.add_argument("--result_only", default=False, action="store_true")
     parser.add_argument("--interval", type=int, default=50)
+
+    # DDIM sampler param
+    parser.add_argument("--eta", type=float, default=0.0)
+    parser.add_argument("--steps", type=int, default=100)
+    parser.add_argument("--method", type=str, default="linear", choices=["linear", "quadratic"])
 
     # save image param
     parser.add_argument("--nrow", type=int, default=4)
@@ -37,13 +43,19 @@ def generate(args):
     model.load_state_dict(cp["model"])
     model.to(device)
 
-    sampler = DDPMSampler(model, **cp["config"]["Trainer"]).to(device)
+    if args.sampler == "ddim":
+        sampler = DDIMSampler(model, **cp["config"]["Trainer"]).to(device)
+    elif args.sampler == "ddpm":
+        sampler = DDPMSampler(model, **cp["config"]["Trainer"]).to(device)
+    else:
+        raise ValueError(f"Unknown sampler: {args.sampler}")
 
     # generate Gaussian noise
     z_t = torch.randn((args.batch_size, cp["config"]["Model"]["in_channels"],
                        *cp["config"]["Dataset"]["image_size"]), device=device)
 
-    x = sampler(z_t, only_return_x_0=args.result_only, interval=args.interval)
+    extra_param = dict(steps=args.steps, eta=args.eta, method=args.method)
+    x = sampler(z_t, only_return_x_0=args.result_only, interval=args.interval, **extra_param)
 
     if args.result_only:
         save_image(x, nrow=args.nrow, show=args.show, path=args.image_save_path, to_grayscale=args.to_grayscale)
--- utils/engine.py
+++ utils/engine.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from tqdm import tqdm
+import numpy as np
 
 
 def extract(v, i, shape):
@@ -101,7 +102,7 @@ class DDPMSampler(nn.Module):
         return x_t_minus_one
 
     @torch.no_grad()
-    def forward(self, x_t, only_return_x_0: bool = True, interval: int = 1):
+    def forward(self, x_t, only_return_x_0: bool = True, interval: int = 1, **kwargs):
         """
         Parameters:
             x_t: Standard Gaussian noise. A tensor with shape (batch_size, channels, height, width).
@@ -110,6 +111,7 @@ class DDPMSampler(nn.Module):
             interval: This parameter is valid only when `only_return_x_0 = False`. Decide the interval at which
                 to save the intermediate process pictures.
                 $x_t$ and $x_0$ will be included, no matter what the value of `interval` is.
+            kwargs: no meaning, just for compatibility.
 
         Returns:
             if `only_return_x_0 = True`, will return a tensor with shape (batch_size, channels, height, width),
@@ -117,14 +119,97 @@ class DDPMSampler(nn.Module):
             include intermediate pictures.
         """
         x = [x_t]
-        with tqdm(reversed(range(self.T)), colour="#6565b5", total=self.T) as time_steps:
-            for time_step in time_steps:
+        with tqdm(reversed(range(self.T)), colour="#6565b5", total=self.T) as sampling_steps:
+            for time_step in sampling_steps:
                 x_t = self.sample_one_step(x_t, time_step)
 
                 if not only_return_x_0 and ((self.T - time_step) % interval == 0 or time_step == 0):
                     x.append(torch.clip(x_t, -1.0, 1.0))
 
-                time_steps.set_postfix(ordered_dict={"step": time_step + 1, "sample": len(x)})
+                sampling_steps.set_postfix(ordered_dict={"step": time_step + 1, "sample": len(x)})
+
+        if only_return_x_0:
+            return x_t  # [batch_size, channels, height, width]
+        return torch.stack(x, dim=1)  # [batch_size, sample, channels, height, width]
+
+
+class DDIMSampler(nn.Module):
+    def __init__(self, model, beta: Tuple[int, int], T: int):
+        super().__init__()
+        self.model = model
+        self.T = T
+
+        # generate T steps of beta
+        beta_t = torch.linspace(*beta, T, dtype=torch.float32)
+        # calculate the cumulative product of $\alpha$ , named $\bar{\alpha_t}$ in paper
+        alpha_t = 1.0 - beta_t
+        self.register_buffer("alpha_t_bar", torch.cumprod(alpha_t, dim=0))
+
+    @torch.no_grad()
+    def sample_one_step(self, x_t, time_step: int, prev_time_step: int, eta: float):
+        t = torch.full((x_t.shape[0],), time_step, device=x_t.device, dtype=torch.long)
+        prev_t = torch.full((x_t.shape[0],), prev_time_step, device=x_t.device, dtype=torch.long)
+
+        # get current and previous alpha_cumprod
+        alpha_t = extract(self.alpha_t_bar, t, x_t.shape)
+        alpha_t_prev = extract(self.alpha_t_bar, prev_t, x_t.shape)
+
+        # predict noise using model
+        epsilon_theta_t = self.model(x_t, t)
+
+        # calculate x_{t-1}
+        sigma_t = eta * torch.sqrt((1 - alpha_t_prev) / (1 - alpha_t) * (1 - alpha_t / alpha_t_prev))
+        epsilon_t = torch.randn_like(x_t)
+        x_t_minus_one = (
+                torch.sqrt(alpha_t_prev / alpha_t) * x_t +
+                (torch.sqrt(1 - alpha_t_prev - sigma_t ** 2) - torch.sqrt(
+                    (alpha_t_prev * (1 - alpha_t)) / alpha_t)) * epsilon_theta_t +
+                sigma_t * epsilon_t
+        )
+        return x_t_minus_one
+
+    @torch.no_grad()
+    def forward(self, x_t, steps: int = 1, method="linear", eta=0.0,
+                only_return_x_0: bool = True, interval: int = 1):
+        """
+        Parameters:
+            x_t: Standard Gaussian noise. A tensor with shape (batch_size, channels, height, width).
+            steps: Sampling steps.
+            method: Sampling method, can be "linear" or "quadratic".
+            eta: Coefficients of sigma parameters in the paper. The value 0 indicates DDIM, 1 indicates DDPM.
+            only_return_x_0: Determines whether the image is saved during the sampling process. if True,
+                intermediate pictures are not saved, and only return the final result $x_0$.
+            interval: This parameter is valid only when `only_return_x_0 = False`. Decide the interval at which
+                to save the intermediate process pictures, according to `step`.
+                $x_t$ and $x_0$ will be included, no matter what the value of `interval` is.
+
+        Returns:
+            if `only_return_x_0 = True`, will return a tensor with shape (batch_size, channels, height, width),
+            otherwise, return a tensor with shape (batch_size, sample, channels, height, width),
+            include intermediate pictures.
+        """
+        if method == "linear":
+            a = self.T // steps
+            time_steps = np.asarray(list(range(0, self.T, a)))
+        elif method == "quadratic":
+            time_steps = (np.linspace(0, np.sqrt(self.T * 0.8), steps) ** 2).astype(np.int)
+        else:
+            raise NotImplementedError(f"sampling method {method} is not implemented!")
+
+        # add one to get the final alpha values right (the ones from first scale to data during sampling)
+        time_steps = time_steps + 1
+        # previous sequence
+        time_steps_prev = np.concatenate([[0], time_steps[:-1]])
+
+        x = [x_t]
+        with tqdm(reversed(range(0, steps)), colour="#6565b5", total=steps) as sampling_steps:
+            for i in sampling_steps:
+                x_t = self.sample_one_step(x_t, time_steps[i], time_steps_prev[i], eta)
+
+                if not only_return_x_0 and ((steps - i) % interval == 0 or i == 0):
+                    x.append(torch.clip(x_t, -1.0, 1.0))
+
+                sampling_steps.set_postfix(ordered_dict={"step": i + 1, "sample": len(x)})
 
         if only_return_x_0:
             return x_t  # [batch_size, channels, height, width]
